1572d8981e9430cd9d4e460c16be5cef372da8a7
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-05-26 21:14:44 +0500

    animation six

--- AnimationSix/AnimationSixApp.swift
+++ AnimationSix/AnimationSixApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct AnimationSixApp: App {
+    @StateObject private var appData = DataModel()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(appData: appData)
         }
     }
 }
---
+++ AnimationSix/Playsound.swift
@@ -0,0 +1,22 @@
+//
+//  Playsound.swift
+//  AnimationSix
+//
+//  Created by Abdusamad Mamasoliyev on 26/05/24.
+//
+
+import Foundation
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("ERROR: Could not find and play the sound file!")
+        }
+    }
+}
