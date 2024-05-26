71af9b913468ac280d3c5a03155dc9e197d66341
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-05-26 21:07:56 +0500

    Initial Commit

---
+++ AnimationSix/AnimationSixApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSixApp.swift
+//  AnimationSix
+//
+//  Created by Abdusamad Mamasoliyev on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationSix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimationSix
+//
+//  Created by Abdusamad Mamasoliyev on 26/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
